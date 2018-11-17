67993ec985f52b119b90383bc9c834fbcc893689
koitt03A <koitt03A@koitt03A-PC>
2018-11-17 15:27:06 +0900

    d

--- src/main/java/com/sns/sp/service/Impl/chatInfoServiceImpl.java
+++ src/main/java/com/sns/sp/service/Impl/chatInfoServiceImpl.java
@@ -2,32 +2,37 @@ package com.sns.sp.service.Impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sns.sp.dao.chatInfoDAO;
 import com.sns.sp.service.chatInfoService;
 import com.sns.sp.vo.chatInfo;
 
 @Service
 public class chatInfoServiceImpl implements chatInfoService {
 
+	@Autowired
+	private chatInfoDAO cidao;
+	
 	@Override
 	public List<chatInfo> selectchatInfoList() {
-		return null;
+		return cidao.selectchatInfoList();
 	}
 
 	@Override
 	public Integer insertchatInfo(chatInfo ci) {
-		return null;
+		return cidao.insertchatInfo(ci);
 	}
 
 	@Override
 	public Integer updatechatInfo(chatInfo ci) {
-		return null;
+		return cidao.updatechatInfo(ci);
 	}
 
 	@Override
 	public Integer deletechatInfo(int chatno) {
-		return null;
+		return cidao.deletechatInfo(chatno);
 	}
 
 }
