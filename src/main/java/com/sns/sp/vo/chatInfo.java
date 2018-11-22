afc0d0ae7d87d624690700b9a8c7befa29cb26e8
koitt03A <koitt03A@koitt03A-PC>
2018-11-22 18:49:32 +0900

    11-22 18:49 최승훈

--- src/main/java/com/sns/sp/vo/chatInfo.java
+++ src/main/java/com/sns/sp/vo/chatInfo.java
@@ -7,5 +7,49 @@ public class chatInfo {
 	private String friendid;
 	private String chattext;
 	private String chatdat;		// date
-	private Integer chatchecked; 
+	private Integer chatchecked;
+	public Integer getChatno() {
+		return chatno;
+	}
+	public void setChatno(Integer chatno) {
+		this.chatno = chatno;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getFriendid() {
+		return friendid;
+	}
+	public void setFriendid(String friendid) {
+		this.friendid = friendid;
+	}
+	public String getChattext() {
+		return chattext;
+	}
+	public void setChattext(String chattext) {
+		this.chattext = chattext;
+	}
+	public String getChatdat() {
+		return chatdat;
+	}
+	public void setChatdat(String chatdat) {
+		this.chatdat = chatdat;
+	}
+	public Integer getChatchecked() {
+		return chatchecked;
+	}
+	public void setChatchecked(Integer chatchecked) {
+		this.chatchecked = chatchecked;
+	}
+	@Override
+	public String toString() {
+		return "chatInfo [chatno=" + chatno + ", userid=" + userid + ", friendid=" + friendid + ", chattext=" + chattext
+				+ ", chatdat=" + chatdat + ", chatchecked=" + chatchecked + "]";
+	} 
+	
+	
 }
+
