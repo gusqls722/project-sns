1936dd2297d9a270b7fcb63079fa0e1b65bcf8b8
koitt03A <koitt03A@koitt03A-PC>
2018-12-15 16:37:31 +0900

    12-15 16:37

---
+++ src/main/java/com/sns/sp/controller/BoardInfoController.java
@@ -0,0 +1,38 @@
+package com.sns.sp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sns.sp.service.BoardInfoService;
+import com.sns.sp.vo.BoardInfo;
+
+@Controller
+public class BoardInfoController {
+
+	@Autowired
+	private BoardInfoService bs;
+	
+	@GetMapping(value="/bis")
+	public @ResponseBody List<BoardInfo> getboardInfoList(@ModelAttribute BoardInfo bi){
+		
+		return bs.selectboardInfoList();
+	}
+	
+	@GetMapping(value="/bis/{boardno}")
+	public @ResponseBody BoardInfo getboardInfoOne(@PathVariable int boardno) {
+		return bs.selectboardInfoOne(boardno);
+	}
+	
+	@PostMapping(value="/bis")
+	public @ResponseBody Integer insertboardInfo(@RequestBody BoardInfo bi) {
+		return bs.insertboardInfo(bi);
+	}
+}
